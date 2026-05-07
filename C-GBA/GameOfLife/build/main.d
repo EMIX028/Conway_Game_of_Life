5df50d61ff7f6351fc8ee66cc8a5aad765363667
EMIX028 <maxime.chl2007@gmail.com>
2026-05-07 23:39:48 +0200

    fonction line algo bresenham

---
+++ C-GBA/GameOfLife/build/main.d
@@ -0,0 +1,12 @@
+main.o: \
+ /home/maxime/Documents/GitGub_Local_repo/Conway_Game_of_Life/C-GBA/GameOfLife/source/main.c \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /opt/devkitpro/libgba/include/gba_video.h \
+ /opt/devkitpro/libgba/include/gba_base.h \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /opt/devkitpro/libgba/include/gba_systemcalls.h
+/opt/devkitpro/libgba/include/gba_types.h:
+/opt/devkitpro/libgba/include/gba_video.h:
+/opt/devkitpro/libgba/include/gba_base.h:
+/opt/devkitpro/libgba/include/gba_types.h:
+/opt/devkitpro/libgba/include/gba_systemcalls.h:
--- C-GBA/GameOfLife/source/main.c
+++ C-GBA/GameOfLife/source/main.c
@@ -1,33 +1,63 @@
-
-#include <gba_console.h>
+#include <gba_types.h>
 #include <gba_video.h>
-#include <gba_interrupt.h>
 #include <gba_systemcalls.h>
-#include <gba_input.h>
-#include <stdio.h>
 #include <stdlib.h>
 
+u16* video = (u16*)VRAM;
 
-int main(void) {
+void placePX(u8 posx, u8 posy, u8 r, u8 g, u8 b){
+    //u8 y = 160 - posy;
+    video[posy*240 + posx] = RGB5(r,g,b);
+}
 
+void drawLine(int x0, int y0, int x1, int y1, u8 r, u8 g, u8 b){
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
 
-	// the vblank interrupt must be enabled for VBlankIntrWait() to work
-	// since the default dispatcher handles the bios flags no vblank handler
-	// is required
-	irqInit();
-	irqEnable(IRQ_VBLANK);
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
 
-	consoleDemoInit();
+    int err = dx - dy;
 
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	BG_COLORS[0] = RGB5(31,0,0);
-	BG_COLORS[241] = RGB5(0,31,0);
-	iprintf("\x1b[10;10HHello World!\n");
+    while (1){
+        placePX(x0, y0, r, g, b);
 
-	while (1) {
-		VBlankIntrWait();
-	}
+        if (x0 == x1 && y0 == y1){
+            break;
+        }
+
+        int e2 = 2 * err;
+
+        if (e2 > -dy){
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx){
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
 
 
+int main(void)
+{
+    REG_DISPCNT = MODE_3 | BG2_ENABLE;
+
+
+    // écran rouge plein
+    for (int i = 0; i < 240 * 160; i++)
+    {
+        video[i] = RGB5(31, 31, 31);
+    }
+    for (int i=0; i<240;i++){
+        placePX(i,3,20,0,0);
+    }
+    drawLine(0,1 ,239 ,160,0,0,0);
+    drawLine(0,160,239 ,0,0,0,0 );
+    while (1)
+    {
+        VBlankIntrWait();
+    }
+}
